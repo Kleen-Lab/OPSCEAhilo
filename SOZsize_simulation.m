c6ca0b15e21a4f0f70ff9d14a83f803404bed492
Jon Kleen <77509587+jkleen@users.noreply.github.com>
2024-10-14 11:38:57 -0700

    Added simulation (two files)

---
+++ SOZsize_simulation.m
@@ -0,0 +1,149 @@
+function [s_vol,e_vol,F]=SOZsize_simulation(dims,r,iedist,XYZshift,sp,color_s,color_e,newfig,initialframe,F)
+% This function simulates what an epileptologist would determine as the SOZ
+% extent based on electrodes that show seizure activity (i.e. in the SOZ)
+% compared to a true (simulated) SOZ. 
+% Procedure:
+%    1. Creates a simulated SOZ in 1, 2, or 3D. 
+%    2. Checks which of the electrodes with coordinates inside that sphere 
+%    and computes their "convex hull" (smallest shape/polygon that fits
+%    all points within its boundary) for them.
+%    3. Calculates volume of the spheree (true SOZ) and the convex hull
+%    (assumed SOZ based on within-sphere electrodes). 
+% Can do this for different recording "dimensions" (1, 2, or 3) to simulate
+% single depth, grid, SEEG/multiple depths respectively.
+
+if ~exist('dims','var') || isempty(dims); dims=3; end % number of dimensions (3 to simulate SEEG/multiple depths)
+if ~exist('r'   ,'var') || isempty(r); r=1;    end % radius in mm
+if ~exist('iedist','var') || isempty(iedist); iedist=5; end % inter-electrode distance in mm
+if ~exist('XYZshift','var') || isempty(XYZshift); XYZshift=[0 0 0]; end % show in figure(s)
+if ~exist('sp','var') || isempty(sp); sp=[1 2 1;1 2 2]; end % specifies the two subplots to show the true (1st row) and estimated (2nd row) SOZ plots
+if ~exist('color_s','var') || isempty(color_s); color_s=[1 1 0]; end %sphere (true SOZ) color
+if ~exist('color_e','var') || isempty(color_e); color_e=[1 0 1]; end %estimated SOZ color
+if ~exist('initialframe','var') || isempty(color_e); initialframe=0; end
+
+ecolor=[0 0 0]; % color of depth electrodes (all)
+eSOZcolor=[1 0 0]; % color of depth electrodes in SOZ
+depthcolor=[.7 .7 .7]; % color of depth probe shaft
+nverts=50; % number of vertices in the sphere (true SOZ)
+s_alpha=.6; %sphere (true SOZ) transparency
+w=5; % zoom out a bit, in mm
+
+if dims==3
+    
+    % create sphere vertices, and scale to radius specified
+    [sX,sY,sZ] = sphere(nverts);
+    sX=sX*r; sX=sX-40;
+    sY=sY*r;
+    sZ=sZ*r;
+    sXYZ = unique([sX(:) sY(:) sZ(:)],'rows');
+    sXYZ(:,1)=sXYZ(:,1)+XYZshift(1);
+    sXYZ(:,2)=sXYZ(:,2)+XYZshift(2);
+    sXYZ(:,3)=sXYZ(:,3)+XYZshift(3);
+    % create convex hull of the 3D points, for the sphere (true SOZ)
+    [sk,s_vol] = convhulln(sXYZ); 
+        s_volume = volume(alphaShape(sXYZ));
+    
+    
+
+    if iedist==5
+        nch=10;
+    elseif iedist==10
+        nch=5; 
+    else; disp('Note: not typical spacing for depths')
+        nch=ceil(45/iedist);
+    end
+    coverage=iedist*(nch-1); % total length of depth in mm
+    n_mm=20; % spacing (in mm) between depths
+    
+    % create first depth
+    depthXs=-coverage/2:iedist:coverage/2;
+    ncd=length(depthXs); %number of channels per depth
+
+    % replicate, to 9 depth probes
+
+    eX=repmat(depthXs,1,9);
+    if iedist==5; eX=eX+2.5; end
+    eX=eX-40; 
+    eY=       [-ones(1,ncd*3) 0*ones(1,ncd*3) ones(1,ncd*3)  ]; % space out 9 depths
+      eY=eY*n_mm; %multiply by number of centimeters between parallel depths
+    eZ=repmat([-ones(1,ncd)   0*ones(1,ncd)   ones(1,ncd)],1,3);
+      eZ=eZ*n_mm; %multiply by number of centimeters between parallel depths
+    eXYZ=[eX' eY' eZ'];
+    
+    % Identify which electrodes are in the true SOZ (sphere volume)
+    einSOZ = inhull(eXYZ,sXYZ);%,tess,tol);
+    % create convex hull of the 3D points, for the interpreted SOZ
+    [ek,e_vol] = convhulln(eXYZ(einSOZ,:)); 
+        e_volume = volume(alphaShape(eXYZ(einSOZ,:)));
+    
+    
+    
+    if sp %plot sphere
+      if newfig; figure('color','w')
+      end
+      
+
+
+      if sp(1,1)>0 % gives option to plot true SOZ (e.g. if doing subplots for a movie)
+        subplot(sp(1,1),sp(1,2),sp(1,3))
+        plot3(eXYZ(:,1),eXYZ(:,2),eXYZ(:,3),'.','color',ecolor,'markersize',20); % plot all electrodes
+        hold on
+        plot3(eXYZ(einSOZ,1),eXYZ(einSOZ,2),eXYZ(einSOZ,3),'.','color',eSOZcolor,'markersize',16); % color electrodes that are in the true SOZ
+        xlabel('X'); ylabel('Y'); zlabel('Z');
+        grid on
+        % connecting lines to visualize the 9 depths
+        chidx=1:nch;
+        for i=1:9
+            plot3(eXYZ(chidx,1),eXYZ(chidx,2),eXYZ(chidx,3),'-','color',depthcolor,'LineWidth',2); 
+            chidx=chidx+nch;
+        end
+        % plot sphere (true SOZ)
+        trisurf(sk,sXYZ(:,1),sXYZ(:,2),sXYZ(:,3))
+        colormap(gca,color_s); 
+        alpha(s_alpha); 
+        shading flat; 
+        lightsout; 
+        litebrain('sal',1)
+        %litebrain('saallll',0)
+        view(215,20)
+        lighting phong
+        axis equal
+        axis([min(eXYZ(:,1))-w max(eXYZ(:,1))+w   min(eXYZ(:,2))-w max(eXYZ(:,2))+w   min(eXYZ(:,3))-w max(eXYZ(:,3))+w])
+      end
+
+        subplot(sp(2,1),sp(2,2),sp(2,3))
+        plot3(eXYZ(:,1),eXYZ(:,2),eXYZ(:,3),'.','color',ecolor,'markersize',20); % plot all electrodes
+        hold on
+        plot3(eXYZ(einSOZ,1),eXYZ(einSOZ,2),eXYZ(einSOZ,3),'.','color',eSOZcolor,'markersize',14); % color electrodes that are in the true SOZ
+        xlabel('X'); ylabel('Y'); zlabel('Z');
+        grid on
+        % connecting lines to visualize the 9 depths
+        chidx=1:nch;
+        for i=1:9
+            plot3(eXYZ(chidx,1),eXYZ(chidx,2),eXYZ(chidx,3),'-','color',depthcolor,'LineWidth',2); 
+            chidx=chidx+nch;
+        end
+        % plot sphere (true SOZ)
+        trisurf(ek,eXYZ(einSOZ,1),eXYZ(einSOZ,2),eXYZ(einSOZ,3));
+        colormap(gca,color_e); 
+        alpha(s_alpha);     
+        shading flat; 
+        lightsout; 
+        litebrain('sal',1)
+        %litebrain('saarrrr',0)
+        view(215,20)
+        lighting phong
+        axis equal
+        axis([min(eXYZ(:,1))-w max(eXYZ(:,1))+w   min(eXYZ(:,2))-w max(eXYZ(:,2))+w   min(eXYZ(:,3))-w max(eXYZ(:,3))+w])
+        
+    end
+    drawnow
+
+        if initialframe==1;
+            clear F; f=1;
+            F(f)=getframe(gcf); 
+            f=f+1;
+        end
+
+end
+
---
+++ SOZsize_simulation_video.m
@@ -0,0 +1,113 @@
+% function SOZsize_simulation_video
+
+dims=3; 
+% r_SOZs=11:.1:24; 
+r_SOZs=18:.1:33; 
+iedist_LD=10;
+iedist_HD=5;
+iedist_HHD=1;
+XYZshift=[2 5 8];
+color_SOZ= [1 1 0];
+color_LD=[1 .3 1];
+color_HD=[.3 .3 1];
+color_HHD=[.3 1 .3];
+
+xl=[2e4 1.52e5];
+yl1=[    0 60000]; %volume in mm^3
+%yl2=[-2000 10000]; %volume in mm^3
+yl2=[-30 90]; %percent change
+nr=length(r_SOZs);
+
+sp_LD=[2,3,1; 2,3,2];
+sp_HD=[2,3,4; 2,3,5];
+s_vol_LD=nan(nr,1);
+s_vol_HD=nan(nr,1);
+s_vol_HHD=nan(nr,1);
+e_vol_LD=nan(nr,1);
+e_vol_HD=nan(nr,1);
+e_vol_HHD=nan(nr,1);
+
+figure('color','w','position',[100 4 1171 736])
+rot8=215;
+F=[];
+for i=1:length(r_SOZs)
+    tic
+    subplot(10,1,1:9) %clear all subplots
+    % low density (LD)                          % (dims,r,        iedist,   XYZshift,sp,   color_s,  color_e, newfig,brainplot,F)
+    [s_vol_LD(i),e_vol_LD(i),F]=SOZsize_simulation(dims,r_SOZs(i),iedist_LD,XYZshift,sp_LD,color_SOZ,color_LD,0,i==1,F);
+    title({['LD estimated volume: ' num2str(round(e_vol_LD(i))) 'mm^3'],''}); 
+    
+    % high density (HD)
+    [s_vol_HD(i),e_vol_HD(i)]=SOZsize_simulation(dims,r_SOZs(i),iedist_HD,XYZshift,sp_HD,color_SOZ,color_HD,0);
+    title({['HD estimated volume: ' num2str(round(e_vol_HD(i))) 'mm^3'],''}); 
+
+    % 2mm density (HD)
+    figure(2); set(gcf,'color','w','Position',[1 4 1171 736]); subplot(10,1,1:9) %clear all subplots
+    [s_vol_HHD(i),e_vol_HHD(i)]=SOZsize_simulation(dims,r_SOZs(i),iedist_HHD,XYZshift,sp_HD,color_SOZ,color_HD,0);
+    title({['HHD estimated volume: ' num2str(round(e_vol_HHD(i))) 'mm^3'],''}); 
+    subplot(2,3,4); title({'HHD condition',['SOZ true volume: ' num2str(round(s_vol_HHD(i))) 'mm^3'],['radius: ' num2str(r_SOZs(i)) 'mm'],''}); 
+    figure(1)
+
+    subplot(2,3,1); title({'LD condition',['SOZ true volume: ' num2str(round(s_vol_LD(i))) 'mm^3'],['radius: ' num2str(r_SOZs(i)) 'mm'],''}); 
+    subplot(2,3,4); title({'HD condition',['SOZ true volume: ' num2str(round(s_vol_HD(i))) 'mm^3'],['radius: ' num2str(r_SOZs(i)) 'mm'],''}); 
+    
+    % plot volumes
+    subplot(8,3,[9:3:18]); 
+    %dots
+    plot(s_vol_LD(1:i),e_vol_LD(1:i),'.-','linewidth',3,'color',color_LD*.7) %estimate SOZ with LD
+    hold on
+    grid on
+    %plot(s_vol_LD(1:i),e_vol_LD(1:i),'.','markersize',15,'color',color_LD*.7) %estimate SOZ with LD
+    %set(gca,'XScale','log','YScale','log')
+    plot(s_vol_LD(1:i),e_vol_HD(1:i),'.-','linewidth',3,'color',color_HD*.7) %estimate SOZ with HD
+    %plot(s_vol_HD(1:i),e_vol_HD(1:i),'.','markersize',15,'color',color_HD*.7) %estimate SOZ with HD
+    %lines
+    %xlim([50e2 1.6e4])
+    plot(s_vol_LD(1:i),e_vol_HHD(1:i),'.-','linewidth',3,'color',color_HHD*.7) %estimate SOZ with HD
+    
+    plot([1 max([xl yl1])],[1 max([xl yl1])],'-','linewidth',.5,'color',[.1 .1 .1]) %marks line where true = estimated (theoretical)
+    plot(s_vol_LD(1:i),s_vol_LD(1:i),'.-','linewidth',3,'color',[.1 .1 .1]) %estimate SOZ with HD
+    xlim(xl)
+    ylim(yl1); ylabel('Estimated volume (mm^3)')
+    text(3e4,3.5e4,'Perfect estimate','Rotation',69)
+    legend({[num2str(iedist_LD) 'mm'],[num2str(iedist_HD) 'mm'],[num2str(iedist_HHD) 'mm']},'Location','southeast')
+    
+    subplot(4,3,12)
+    %dots
+    %plot(s_vol_LD(1:i),e_vol_HD(1:i)-e_vol_LD(1:i),'.-','linewidth',4,'color',mean([color_LD;color_HD],1)*.8) %estimate SOZ with LD
+    plot(s_vol_LD(1:i),(e_vol_HD (1:i)-e_vol_LD(1:i))./e_vol_LD(1:i)*100,'.-','linewidth',4,'color',color_HD*.8) %estimate SOZ with LD
+    hold on
+    grid on
+    plot(s_vol_LD(1:i),(e_vol_HHD(1:i)-e_vol_LD(1:i))./e_vol_LD(1:i)*100,'.-','linewidth',3,'color',color_HHD*.8) %estimate SOZ with LD
+    plot(xl,[0 0],'k-','linewidth',2) 
+    
+    %plot(s_vol_LD(1:i),e_vol_HD(1:i)-e_vol_LD(1:i),'-','markersize',15,'color',mean([color_LD;color_HD],1)*.8) %estimate SOZ with LD
+    %set(gca,'XScale','log','YScale','log')
+    %title('HD minus LD (mm^3)')
+    xlabel('SOZ true volume (mm^3)')
+    ylabel({['HD volume % difference'],['relative to LD (' num2str(iedist_LD) 'mm)']})
+    %lines
+    %xlim([50e2 1.6e4])
+    xlim(xl)
+    ylim(yl2)
+    legend({[num2str(iedist_HD) 'mm'],[num2str(iedist_HHD) 'mm']},'Location','northeast')
+
+    for s=[1 2 4 5];
+        subplot(2,3,s);
+        set(gca,'Clipping','off')
+        view(rot8,20);
+    end
+    rot8=rot8+.2;
+    
+    F(length(F)+1)=getframe(gcf); 
+    toc
+end
+
+vidfn = ['~/Desktop/OPSCEAhilo_sim_vids/' jkdatetime];
+v=VideoWriter(vidfn,'MPEG-4');
+v.FrameRate = 10;
+open(v);
+writeVideo(v,F);
+close(v);
+
+
